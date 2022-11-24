7c1750658b9c4ae649053f3a1ca7f312be541479
sub1120 <subhamsahu1120@gmail.com>
2022-11-24 09:23:45 +0530

    remove list and create seperate list for todo and task

--- client/src/components/Todo/TodoList.js
+++ client/src/components/Todo/TodoList.js
@@ -1,9 +1,18 @@
-import List from "../List";
-
 const TodoList = () => {
   return (
     <div className="border-slate-200 border-2 rounded-lg h-full">
-      <List heading="Todos"></List>
+      <table className="table-auto border-collapse w-full">
+        <thead>
+          <tr className="text-slate-800">
+            <th className="p-2">Todos</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="border-slate-200 border-t-2 text-slate-800">
+            <td className="p-2"></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
