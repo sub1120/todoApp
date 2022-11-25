9348b48f2e2fceb75ffdbeec6ed4bfc9c2fefa99
sub1120 <subhamsahu1120@gmail.com>
2022-11-25 12:05:33 +0530

    update tasklist using li element

--- client/src/components/Task/TaskList.js
+++ client/src/components/Task/TaskList.js
@@ -20,48 +20,38 @@ const TaskList = ({ tasks }) => {
 
   return (
     <div className="border-slate-200 border-2 rounded-lg h-full">
-      <table className="table-auto border-collapse w-full">
-        <thead>
-          <tr>
-            <th className="p-2">Tasks</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tasks.map((task) => (
-            <tr className="border-slate-200 border-t-2">
-              <td>
-                <div className="flex ">
-                  <div className="flex-auto mr-2">
-                    <Task
-                      taskName={task.name}
-                      iseditMode={iseditMode}
-                      disabled={iseditMode ? false : true}
-                    />
-                  </div>
-                  <div className="mx-2">
-                    <button
-                      className={`w-fit h-full text-blue-800 rounded-md`}
-                      onClick={iseditMode ? saveHandler : editHandler}
-                    >
-                      {iseditMode ? "Save" : "Edit"}
-                    </button>
-                  </div>
-                  <div className="mx-2">
-                    <button
-                      className={`w-fit h-full text-red-800 rounded-md`}
-                      onClick={iseditMode ? cancelHandler : deleteHandler}
-                    >
-                      {iseditMode ? "Cancel" : "Delete"}
-                    </button>
-                  </div>
-                </div>
-              </td>
-            </tr>
-          ))}
+      <ul className="table-auto border-collapse w-full">
+        <li className="p-2">Tasks</li>
+        {tasks.map((task) => (
+          <li className="border-slate-300 border-t-2 flex">
+            <div className="flex-auto mr-2">
+              <Task
+                taskName={task.name}
+                iseditMode={iseditMode}
+                disabled={iseditMode ? false : true}
+              />
+            </div>
+            <div className="mx-2">
+              <button
+                className={`w-fit h-full text-blue-800 rounded-md`}
+                onClick={iseditMode ? saveHandler : editHandler}
+              >
+                {iseditMode ? "Save" : "Edit"}
+              </button>
+            </div>
+            <div className="mx-2">
+              <button
+                className={`w-fit h-full text-red-800 rounded-md`}
+                onClick={iseditMode ? cancelHandler : deleteHandler}
+              >
+                {iseditMode ? "Cancel" : "Delete"}
+              </button>
+            </div>
+          </li>
+        ))}
 
-          <tr className="border-slate-200 border-t-2 text-slate-800"></tr>
-        </tbody>
-      </table>
+        <li className="border-slate-200 border-t-2 text-slate-800"></li>
+      </ul>
     </div>
   );
 };
