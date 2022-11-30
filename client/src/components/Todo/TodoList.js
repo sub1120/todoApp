b4d9cb4c55d551ba65a69c4637d2122eac9dd10e
sub1120 <subhamsahu1120@gmail.com>
2022-11-30 11:13:58 +0530

    clean todolist.js

--- client/src/components/Todo/TodoList.js
+++ client/src/components/Todo/TodoList.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const TodoList = ({ todos, appDispatch }) => {
-  const todoSelectHandler = async (e) => {
+  const onClickHandler = async (e) => {
     try {
       const res = await axios.get(
         `http://localhost:4000/getTodo/${e.target.id}`
@@ -24,7 +24,7 @@ const TodoList = ({ todos, appDispatch }) => {
         <li className="text-slate-800 p-2">Todos</li>
         {todos.map((todo) => (
           <li
-            onClick={todoSelectHandler}
+            onClick={onClickHandler}
             id={todo.id}
             key={todo.id}
             className="border-slate-200 border-t-2 text-slate-800 active:bg-blue-600 active:text-white cursor-pointer px-4 py-2"
