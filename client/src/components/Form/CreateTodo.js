2147b971c26d041ceea70031b42ac318239ede09
sub1120 <subhamsahu1120@gmail.com>
2022-11-30 09:03:33 +0530

    clean createtodo.js

--- client/src/components/Form/CreateTodo.js
+++ client/src/components/Form/CreateTodo.js
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import axios from "axios";
-import Button from "../Button";
 
 const CreateTodo = ({ appDispatch }) => {
   const [title, setTitle] = useState("");
 
-  const submitFormHandler = async (event) => {
+  const onSubmitHandler = async (event) => {
     try {
       event.preventDefault();
       const data = {
@@ -25,25 +24,25 @@ const CreateTodo = ({ appDispatch }) => {
     }
   };
 
-  const titleHandler = (event) => {
+  const onChangeHandler = (event) => {
     setTitle(event.target.value);
   };
 
   return (
     <div>
-      <form onSubmit={submitFormHandler}>
+      <form onSubmit={onSubmitHandler}>
         <div className="sm:flex sm:space-x-4">
           <div className="my-2 sm:flex-auto sm:basis-4/6">
             <input
-              className={`p-3 w-full h-14 border-2 border-slate-300 rounded-md focus:outline-none`}
+              className="p-3 w-full h-14 border-2 border-slate-300 rounded-md focus:outline-none"
               placeholder="Title"
               value={title}
-              onChange={titleHandler}
+              onChange={onChangeHandler}
             ></input>
           </div>
           <div className="my-2 sm:flex-auto sm:basis-2/6">
             <button
-              className={`w-full h-14 bg-blue-600 hover:bg-blue-800 active:bg-blue-600 text-white rounded-md`}
+              className="w-full h-14 bg-blue-600 hover:bg-blue-800 active:bg-blue-600 text-white rounded-md"
               type="submit"
             >
               Create Todo
