1dabec1fdc08f31a8d717adf205c2e11a623e9ca
sub1120 <subhamsahu1120@gmail.com>
2022-12-07 15:46:02 +0530

    remove extra div in createTodo

--- client/src/components/Form/CreateTodo.js
+++ client/src/components/Form/CreateTodo.js
@@ -2,28 +2,23 @@ import { Form } from "react-router-dom";
 
 const CreateTodo = () => {
   return (
-    <div>
-      <Form method="post" action="new">
-        <div className="sm:flex sm:space-x-4">
-          <div className="my-2 sm:flex-auto sm:basis-4/6">
-            <input
-              className="p-3 w-full h-14 border-2 border-slate-300 rounded-md focus:outline-none"
-              placeholder="Title"
-              name="title"
-              defaultValue=""
-            ></input>
-          </div>
-          <div className="my-2 sm:flex-auto sm:basis-2/6">
-            <button
-              className="w-full h-14 bg-blue-600 hover:bg-blue-800 active:bg-blue-600 text-white rounded-md"
-              type="submit"
-            >
-              Create Todo
-            </button>
-          </div>
+    <Form method="post" action="new">
+      <div className="sm:flex sm:space-x-4">
+        <div className="my-2 sm:flex-auto sm:basis-4/6">
+          <input
+            className="p-3 w-full h-14 border-2 border-slate-300 rounded-md focus:outline-none"
+            placeholder="Title"
+            name="title"
+            defaultValue=""
+          ></input>
         </div>
-      </Form>
-    </div>
+        <div className="my-2 sm:flex-auto sm:basis-2/6">
+          <button className="w-full h-14 bg-blue-600 hover:bg-blue-800 active:bg-blue-600 text-white rounded-md">
+            Create Todo
+          </button>
+        </div>
+      </div>
+    </Form>
   );
 };
 
