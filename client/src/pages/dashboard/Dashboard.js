cc2edb93ead56a0c7ddfa135bd947cd95b7b605f
sub1120 <subhamsahu1120@gmail.com>
2022-12-07 13:42:09 +0530

    remove createTodoPanel

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import CreateTodoPanel from "../../components/Panel/CreateTodoPanel";
+import CreateTodo from "../../components/Form/CreateTodo";
 import TodoList from "../../components/Todo/TodoList";
 import { Outlet, useLoaderData } from "react-router-dom";
 
@@ -9,7 +9,7 @@ const Dashboard = () => {
   return (
     <div className="m-5 flex flex-col space-y-4 lg:flex-row lg:space-x-4 lg:space-y-0">
       <div className="flex flex-col h-screen lg:basis-1/2">
-        <CreateTodoPanel></CreateTodoPanel>
+        <CreateTodo></CreateTodo>
         <TodoList todos={todoData}></TodoList>
       </div>
       <div className="flex flex-col h-screen border-2 p-6 rounded-md lg:basis-1/2">
